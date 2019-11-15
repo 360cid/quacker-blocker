2d9188aa69083485ff14193c53abcb81f77e7873
360cid <360cid@gmail.com>
2019-11-14 20:41:54 -0800

    Ditch title update as it's inaccurate when switching domains

--- ui/popup.js
+++ ui/popup.js
@@ -18,14 +18,11 @@ const ui = {
 const showBlockingUI = () => {
   ui.statusLabel.innerText = appText.STATUS_ENABLED
   ui.statusToggleContainer.classList.add(classnames.STATUS_ENABLED)
-  chrome.browserAction.setTitle({ title: 'Quacker Blocker is enabled' })
 }
 
 const showDisabledUI = () => {
   ui.statusLabel.innerText = appText.STATUS_DISABLED
   ui.statusToggleContainer.classList.remove(classnames.STATUS_ENABLED)
-  chrome.browserAction.setTitle({ title: 'Quacker Blocker is disabled' })
-  // TODO: show inactive browser icon
 }
 
 let App
@@ -34,7 +31,7 @@ chrome.runtime.getBackgroundPage((window) => {
   if (!window.App) { throw new Error('window.App is undefined') }
   App = window.App
 
-  // Update UI if App is enabled
+  // Update UI if blocking is enabled for the current domain
   App.getCurrentTab((tab) => {
     const { url } = tab
     const domain = getDomainFromUrl(url)
